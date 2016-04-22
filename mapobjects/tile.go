d2d7be27d72874c64eea646a3fb616c7c3d46bd8
Sergey Golovin <golovim@gmail.com>
2016-04-22 13:40:15 +0300

    add simple tests

--- mapobjects/tile.go
+++ mapobjects/tile.go
@@ -5,27 +5,27 @@ import (
 )
 
 type Tile struct {
-	Z    int
-	X    int
-	Y    int
-	Lat  float64
-	Long float64
+	Z   int
+	X   int
+	Y   int
+	Lat float64
+	Lon float64
 }
 
 type Conversion interface {
 	deg2num(t *Tile) (x int, y int)
-	num2deg(t *Tile) (lat float64, long float64)
+	num2deg(t *Tile) (lat float64, lon float64)
 }
 
 func (*Tile) Deg2num(t *Tile) (x int, y int) {
-	x = int(math.Floor((t.Long + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
+	x = int(math.Floor((t.Lon + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
 	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
 	return
 }
 
-func (*Tile) Num2deg(t *Tile) (lat float64, long float64) {
+func (*Tile) Num2deg(t *Tile) (lat float64, lon float64) {
 	n := math.Pi - 2.0*math.Pi*float64(t.Y)/math.Exp2(float64(t.Z))
 	lat = 180.0 / math.Pi * math.Atan(0.5*(math.Exp(n)-math.Exp(-n)))
-	long = float64(t.X)/math.Exp2(float64(t.Z))*360.0 - 180.0
-	return lat, long
+	lon = float64(t.X)/math.Exp2(float64(t.Z))*360.0 - 180.0
+	return lat, lon
 }
--- mapobjects/tile_test.go
+++ mapobjects/tile_test.go
@@ -1,7 +1,24 @@
 package mapobjects
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestDeg2NumConversion(t *testing.T) {
-	tile := Tile{Z: 12, X: 2468, Y: 1273}
+	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866769, Lon: 37.529296875}
+
+	x, y := tile.Deg2num(&tile)
+
+	assert.Equal(t, x, tile.X, "should be equal")
+	assert.Equal(t, y, tile.Y, "should be equal")
+}
+
+func TestNum2degConversion(t *testing.T) {
+	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866769, Lon: 37.529296875}
+
+	lat, lon := tile.Num2deg(&tile)
+
+	assert.Equal(t, lat, tile.Lat, "should be equal")
+	assert.Equal(t, lon, tile.Lon, "should be equal")
 }
