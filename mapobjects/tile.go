935a57cb1d79e810e42537af982a5681529d1ed2
alexeysidash <alexeysidash@gmail.com>
2016-04-26 16:23:17 +0300

    Added some tests and comments. Remove some unnecessaru code.

--- mapobjects/tile.go
+++ mapobjects/tile.go
@@ -3,24 +3,10 @@ package mapobjects
 import (
 	"math"
 )
-/*
- Size of each tile in pixels
- */
-const TILE_SIZE = 256;
+/* Size of each tile in pixels */
+const TILE_SIZE = 256.0;
 
-/*
- Contains tile properties
-
- Z,X,Y - tile coordinates according to OSM specs(see http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames)
- Bounding box - geographical coordinates of each side of tile
- */
-type Tile struct {
-	Z, X, Y     int
-	Lat         float64
-	Lon         float64
-	BoundingBox BoundingBox
-}
-
+// region BoundingBox
 /*
  Contains the most north/south/east/west coordinates of tile.
  */
@@ -29,6 +15,29 @@ type BoundingBox struct {
 	East  float64
 	South float64
 	West  float64
+}
+
+/*
+ Takes point latitude and longitude and returns true if this point is inside of this BoundingBox.
+ */
+func (bbox BoundingBox) Contains(lat, lon float64) bool {
+	return (bbox.North >= lat && bbox.South <= lat) && (bbox.West <= lon && bbox.East >= lon)
+}
+// endregion
+
+// region Tile
+/*
+ Contains tile properties
+
+ Z,X,Y - tile coordinates according to OSM specs(see http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames)
+
+ Bounding box - geographical coordinates of each side of tile
+ */
+type Tile struct {
+	Z, X, Y     int
+	Lat         float64
+	Lon         float64
+	BoundingBox BoundingBox
 }
 
 /*
@@ -46,23 +55,6 @@ func Tile2lat(y int, z int) float64 {
 	return math.Atan(math.Sinh(float64(n))) * 180 / math.Pi;
 }
 
-type Conversion interface {
-	deg2num(t *Tile) (x int, y int)
-	num2deg(t *Tile) (lat float64, lon float64)
-}
-
-func (*Tile) Deg2num(t *Tile) (x int, y int) {
-	x = int(math.Floor((t.Lon + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
-	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat * math.Pi / 180.0) + 1.0 / math.Cos(t.Lat * math.Pi / 180.0)) / math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
-	return
-}
-
-func (*Tile) Num2deg(t *Tile) (lat float64, lon float64) {
-	lat = Tile2lat(t.Y, t.Z)
-	lon = Tile2lon(t.X, t.Z)
-	return lat, lon
-}
-
 /*
  Tile factory function
  */
@@ -76,3 +68,28 @@ func NewTile(x int, y int, z int) Tile {
 			East : Tile2lon(x + 1, z),
 		}}
 }
+
+/*
+ Takes point latitude and longitude and returns pixel coordinates of point on some tile.
+
+ May return negative values as well as values outside of tile
+ */
+func (tile Tile) Degrees2Pixels(lat, lon float64) (x float64, y float64) {
+	northToSouthResolution := math.Abs(tile.BoundingBox.North - tile.BoundingBox.South) / TILE_SIZE
+	eastToWestResolution := math.Abs(tile.BoundingBox.East - tile.BoundingBox.West) / TILE_SIZE
+
+	deltaLat := tile.BoundingBox.North - lat
+	deltaLon := lon - tile.BoundingBox.West
+	x = deltaLon / eastToWestResolution
+	y = deltaLat / northToSouthResolution
+	return x, y
+}
+
+/*
+ Takes point latitude and longitude and returns true if this point is present on this tile.
+ */
+func (tile Tile) Contains(lat, lon float64) bool {
+	return tile.BoundingBox.Contains(lat, lon)
+}
+
+// endregion
--- mapobjects/tile_test.go
+++ mapobjects/tile_test.go
@@ -5,24 +5,6 @@ import (
 	"testing"
 )
 
-func TestDeg2NumConversion(t *testing.T) {
-	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866769, Lon: 37.529296875}
-
-	x, y := tile.Deg2num(&tile)
-
-	assert.Equal(t, tile.X, x, "should be equal")
-	assert.Equal(t, tile.Y, y, "should be equal")
-}
-
-func TestNum2degConversion(t *testing.T) {
-	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866768, Lon: 37.529296875}
-
-	lat, lon := tile.Num2deg(&tile)
-
-	assert.Equal(t, tile.Lat, lat, "should be equal")
-	assert.Equal(t, tile.Lon, lon, "should be equal")
-}
-
 func TestNewTile(t *testing.T) {
 	tile := NewTile(0, 0, 0)
 	assert.Equal(t, tile.BoundingBox.North, 85.05112877980659)
@@ -35,5 +17,24 @@ func TestNewTile(t *testing.T) {
 	assert.Equal(t, tile.BoundingBox.South, 55.727110085045986)
 	assert.Equal(t, tile.BoundingBox.East, 37.6171875)
 	assert.Equal(t, tile.BoundingBox.West, 37.529296875)
+}
 
+func TestTile_Contains(t *testing.T) {
+	tile := NewTile(0, 0, 0)
+	assert.True(t, tile.Contains(0, 0), "this point is present on this tile")
+
+	tile = NewTile(0, 1, 1)
+	assert.False(t, tile.Contains(1, 1), "this point is not present on this tile")
+}
+
+func TestTile_Degrees2Pixels(t *testing.T) {
+	tile := NewTile(0, 0, 0)
+	x, y := tile.Degrees2Pixels(0, 0)
+	assert.Equal(t, TILE_SIZE / 2, x, "point with (0,0) coords should be exactly in the center of whole world tile")
+	assert.Equal(t, TILE_SIZE / 2, y, "point with (0,0) coords should be exactly in the center of whole world tile")
+
+	tile = NewTile(1, 0, 1)
+	x, y = tile.Degrees2Pixels(0, 0)
+	assert.Equal(t, 0.0, x, "point with (0,0) coords should be exactly in the center of whole world tile")
+	assert.Equal(t, TILE_SIZE, y, "point with (0,0) coords should be exactly in the center of whole world tile")
 }
