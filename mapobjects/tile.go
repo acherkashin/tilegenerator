30e6c2b91e038f4096095ebc868a416c1426ab7c
alexeysidash <alexeysidash@gmail.com>
2016-04-26 14:52:27 +0300

    Added tile factory function.

--- mapobjects/tile.go
+++ mapobjects/tile.go
@@ -5,11 +5,28 @@ import (
 )
 
 type Tile struct {
-	Z   int
-	X   int
-	Y   int
-	Lat float64
-	Lon float64
+	Z           int
+	X           int
+	Y           int
+	Lat         float64
+	Lon         float64
+	BoundingBox BoundingBox
+}
+
+type BoundingBox struct {
+	North float64
+	East  float64
+	South float64
+	West  float64
+}
+
+func Tile2lon(x int, z int) float64 {
+	return float64(x) / math.Pow(2.0, float64(z)) * 360.0 - 180.0;
+}
+
+func Tile2lat(y int, z int) float64 {
+	n := math.Pi - (2.0 * math.Pi * float64(y)) / math.Pow(2.0, float64(z));
+	return math.Atan(math.Sinh(float64(n))) * 180 / math.Pi;
 }
 
 type Conversion interface {
@@ -19,13 +36,25 @@ type Conversion interface {
 
 func (*Tile) Deg2num(t *Tile) (x int, y int) {
 	x = int(math.Floor((t.Lon + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
-	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
+	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat * math.Pi / 180.0) + 1.0 / math.Cos(t.Lat * math.Pi / 180.0)) / math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
 	return
 }
 
 func (*Tile) Num2deg(t *Tile) (lat float64, lon float64) {
-	n := math.Pi - 2.0*math.Pi*float64(t.Y)/math.Exp2(float64(t.Z))
-	lat = 180.0 / math.Pi * math.Atan(0.5*(math.Exp(n)-math.Exp(-n)))
-	lon = float64(t.X)/math.Exp2(float64(t.Z))*360.0 - 180.0
+	lat = Tile2lat(t.Y, t.Z)
+	lon = Tile2lon(t.X, t.Z)
 	return lat, lon
+}
+
+func NewTile(x int, y int, z int) *Tile {
+	tile := new(Tile)
+	tile.X = x
+	tile.Y = y
+	tile.Z = z
+	tile.BoundingBox = BoundingBox{}
+	tile.BoundingBox.North = Tile2lat(y, z)
+	tile.BoundingBox.South = Tile2lat(y + 1, z)
+	tile.BoundingBox.West = Tile2lon(x, z)
+	tile.BoundingBox.East = Tile2lon(x + 1, z)
+	return tile
 }
--- mapobjects/tile_test.go
+++ mapobjects/tile_test.go
@@ -10,15 +10,24 @@ func TestDeg2NumConversion(t *testing.T) {
 
 	x, y := tile.Deg2num(&tile)
 
-	assert.Equal(t, x, tile.X, "should be equal")
-	assert.Equal(t, y, tile.Y, "should be equal")
+	assert.Equal(t, tile.X, x, "should be equal")
+	assert.Equal(t, tile.Y, y, "should be equal")
 }
 
 func TestNum2degConversion(t *testing.T) {
-	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866769, Lon: 37.529296875}
+	tile := Tile{Z: 12, X: 2475, Y: 1280, Lat: 55.77657301866768, Lon: 37.529296875}
 
 	lat, lon := tile.Num2deg(&tile)
 
-	assert.Equal(t, lat, tile.Lat, "should be equal")
-	assert.Equal(t, lon, tile.Lon, "should be equal")
+	assert.Equal(t, tile.Lat, lat, "should be equal")
+	assert.Equal(t, tile.Lon, lon, "should be equal")
+}
+
+func TestNewTile(t *testing.T) {
+	tile := NewTile(0, 0, 0)
+
+	assert.Equal(t, tile.BoundingBox.North, 85.05112877980659)
+	assert.Equal(t, tile.BoundingBox.South, -85.05112877980659)
+	assert.Equal(t, tile.BoundingBox.East, 180.0)
+	assert.Equal(t, tile.BoundingBox.West, -180.0)
 }
