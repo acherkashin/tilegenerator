1e6a8d47edcf860018b89b1d2fe4d838073db793
Sergey Golovin <golovim@gmail.com>
2016-04-27 21:52:16 +0300

    add some comments

---
+++ database/database.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/terrafactory/tilegenerator/geo"
+)
+
+type GeometryDB struct {
+	conn      *sql.DB
+	geomtable string
+	geomcol   string
+}
+
+// Transfer raw sql rows into a slice of BaseGeometry structs
+func (gdb *GeometryDB) rowsToGeometries(rows *sql.Rows) []geo.BaseGeometry {
+	geometries := []geo.BaseGeometry{}
+	geometry := geo.BaseGeometry{}
+	tmpRows := *rows
+	defer tmpRows.Close()
+
+	for tmpRows.Next() {
+		err := tmpRows.Scan(&geometry.Id, &geometry.Value)
+		if err == nil {
+			geometries = append(geometries, geometry)
+		}
+	}
+	return geometries
+}
+
+// Create db connection. Use "geotable" parameter as a table with geometries and
+// "geocol" as a geometry column
+func (gdb *GeometryDB) InitConnection(username string, connstring string, geomtable string, geomcol string) {
+	db, err := sql.Open(username, connstring)
+	if err == nil {
+		gdb.conn = db
+		gdb.geomtable = geomtable
+		gdb.geomcol = geomcol
+	} else {
+		fmt.Printf("Database connection error: %v\n", err)
+		panic("DB Error")
+	}
+}
+
+// Return slice of all geometries in a database
+func (gdb *GeometryDB) GetAllGeometries() (geometries []geo.BaseGeometry, err error) {
+	q := fmt.Sprintf("SELECT id, ST_AsText( ST_Transform( %s, 4326 ) ) from %s;", gdb.geomcol, gdb.geomtable)
+	rows, err := gdb.conn.Query(q)
+	if err == nil {
+		geometries := gdb.rowsToGeometries(rows)
+		return geometries, err
+	} else {
+		fmt.Printf("Query error: %v", err)
+	}
+	return
+}
