2ab61886c3560d758ba70b85baff631524bb4fb9
alexeysidash <alexeysidash@gmail.com>
2016-05-30 15:11:34 +0300

    Added some cli flags.

--- app.go
+++ app.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/TerraFactory/tilegenerator/listeners"
 	"github.com/TerraFactory/tilegenerator/settings"
+	"os"
+	"fmt"
 )
 
-func main(){
-	conf := settings.GetSettings()
+func main() {
+	var help = flag.Bool("h", false, "Display this message.")
+	var conf_path = flag.String("c", "./config.toml", "Absolute path to configuration file")
+	flag.Parse()
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+		return
+	}
+	conf, err := settings.GetSettings(conf_path)
+
+	if(err != nil) {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
 	listeners.Listen(conf)
 }
