100c287957d887083e5ba2b7902ae2216d6ec1f5
Sergey Golovin <golovim@gmail.com>
2016-06-08 21:45:07 +0300

    align an image primitive inside a Point object

--- settings/styling/primitives/image.go
+++ settings/styling/primitives/image.go
@@ -32,7 +32,7 @@ func (img ImagePrimitive) Render(svg *svg.SVG, geo geometry.Geometry, object *en
 			int(math.Floor(point.Coordinates.X+.5)),
 			int(math.Floor(point.Coordinates.Y+0.5)),
 			img.Rotate)
-		svg.Image(0, 0, int(img.Width), int(img.Height), "data:"+img.Format+";base64,"+inlineBase64Img)
+		svg.Image(-int(img.Width)/2, -int(img.Height)/2, int(img.Width), int(img.Height), "data:"+img.Format+";base64,"+inlineBase64Img)
 		svg.Gend()
 	} else {
 		fmt.Errorf("Can't render %s because of err: '%s'", resultHref, err.Error())
