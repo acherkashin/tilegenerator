904c6e9eb812c8c963765aab9ace46929ab28b55
Sergey Golovin <golovim@gmail.com>
2016-06-14 15:23:18 +0300

    tmp solution for text position changing

--- settings/styling/primitives/text.go
+++ settings/styling/primitives/text.go
@@ -1,12 +1,10 @@
 package primitives
 
 import (
-	"math"
 	"strings"
 
 	"github.com/TerraFactory/svgo"
 	"github.com/TerraFactory/tilegenerator/database/entities"
-	"github.com/TerraFactory/wktparser/geometry"
 )
 
 type TextPrimitive struct {
@@ -17,11 +15,31 @@ type TextPrimitive struct {
 	Content  string
 }
 
-func (text TextPrimitive) Render(svg *svg.SVG, geo geometry.Geometry, object *entities.MapObject) {
-	point, _ := geo.AsPoint()
+func (text TextPrimitive) Render(svg *svg.SVG, object *entities.MapObject) {
+	point, _ := object.Geometry.AsPoint()
+	//Temporary solution. I used static shift value because we need to know image size, but we don't know it
+	// we have to move such primitives as labels and so on into the image primitive
+	var xShift float64
+	var yShift float64
+
+	switch text.Position {
+	case "top":
+		xShift = -20
+		yShift = -40
+	case "bottom":
+		xShift = -20
+		yShift = 40
+	case "left":
+		xShift = -60
+		yShift = 10
+	case "right":
+		xShift = 20
+		yShift = 10
+	}
+
 	svg.Text(
-		int(math.Floor(point.Coordinates.X+.5)),
-		int(math.Floor(point.Coordinates.Y+.5)),
+		int(point.Coordinates.X+xShift),
+		int(point.Coordinates.Y+yShift),
 		text.Content)
 }
 
