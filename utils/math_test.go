cf563411f3be9c484614f717451abbc283894a54
Черкашин Александр Иванович <cherkashin1996@outlook.com>
2016-08-19 04:04:02 +0300

    add test for method LineCenter

--- utils/math_test.go
+++ utils/math_test.go
@@ -39,3 +39,20 @@ func TestLengthPolyline(t *testing.T) {
 		assert.Equal(t, item.expected, length)
 	}
 }
+
+func TestLineCenter(t *testing.T) {
+	var centerTest = []struct {
+		x1, y1, x2, y2       float64
+		expectedX, expectedY float64
+	}{
+		{x1: -10, y1: -25, x2: 10, y2: 25, expectedX: 0, expectedY: 0},
+		{x1: 30, y1: -10, x2: 10, y2: 16, expectedX: 20, expectedY: 3},
+		{x1: 10, y1: 20, x2: 10, y2: 20, expectedX: 10, expectedY: 20},
+	}
+
+	for _, item := range centerTest {
+		centerX, centerY := utils.LineCenter(item.x1, item.y1, item.x2, item.y2)
+		assert.Equal(t, item.expectedX, centerX)
+		assert.Equal(t, item.expectedY, centerY)
+	}
+}
